2298c990f21ff4823c16639fd4ed166a30a07140
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-11 12:42:47 +0530

    age of harry

---
+++ Day 1/level 1/AgeOfHarry.java
@@ -0,0 +1,19 @@
+class AgeOfHarry{ 
+
+public static void main (String args[]){
+//present year 
+int currYear = 2024 ;
+//harry's birth year
+int birthYear = 2000;
+
+//calculating age of harry
+int age = currYear - birthYear ;
+
+//printing age
+System.out.println("Harry's age is : " + age );
+
+}
+
+}
+
+
