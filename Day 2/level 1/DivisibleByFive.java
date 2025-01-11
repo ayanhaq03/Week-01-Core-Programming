4c15531cdf41eb773e7c4c9c33c33cd17d6a5dbd
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-11 15:33:35 +0530

    program to find number which is divisble by 5

---
+++ Day 2/level 1/DivisibleByFive.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class DivisibleByFive{
+
+
+public static void main (String args[]){
+ 
+//taking number input from user
+Scanner input =  new Scanner (System.in) ;
+
+int number = input.nextInt();
+boolean ans = false;
+if(number%5 == 0 ){
+ true;
+}
+
+System.out.println("is the number " + number + " divisible by 5 ?  " + ans );
+
+
+}
+
+}
