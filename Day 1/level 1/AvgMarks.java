30bba7d0bfaa9121effddf0f098e7bae7600cedb
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-11 12:44:33 +0530

    find average of marks

---
+++ Day 1/level 1/AvgMarks.java
@@ -0,0 +1,18 @@
+class AvgMarks{
+
+public static void main(String args[]) {
+
+//marks of physics , chemistry and maths
+int mathMarks = 94 , physicsMarks = 95 ,chemistryMarks = 96 ;
+
+//calculating average percentage of pcm
+int avgPercent = (mathMarks + physicsMarks + chemistryMarks ) *100/ 300 ;
+
+
+System.out.println( "Sam's average  marks in pcm is   " + avgPercent );
+
+
+}
+
+
+}
