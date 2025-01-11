406c92e87213a276e84f3854bc4a8cb2062c57d6
Mohd Ayan haque <ayanhaque904@gmail.com>
2025-01-11 12:45:57 +0530

    convert km to miles

---
+++ Day 1/level 1/KmToMiles.java
@@ -0,0 +1,16 @@
+class KmToMiles{
+
+
+public static void main (String args[] ) {
+	
+	
+//distance in kilometer
+double disInKm = 10.8 ;
+//converting distance to miles
+double disInMiles = disInKm * 1.6 ;
+
+System.out.println("The distance "+disInKm+" in miles is  " + disInMiles );
+
+}
+
+}
